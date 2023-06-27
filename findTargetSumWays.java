12e7f8aceb33ebfcc626a3bb798f67992aa9d4d0
arpensefocong20212em <137243537+arpensefocong20212em@users.noreply.github.com>
2023-06-27 14:10:47 +0800

    Create findTargetSumWays.java
    目标和

---
+++ findTargetSumWays.java
@@ -0,0 +1,28 @@
+/*
+        leetcode:494 目标和
+     */
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+        if ((target + sum) % 2 != 0) {
+            return 0;
+        }
+        int size = (target + sum) / 2;
+        if (size < 0) {
+            size = -size;
+        }
+        //填满j（包括j）这么大容积的包，有dp[j]种方法
+        int[] dp = new int[size + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = size; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[size];
+    }
